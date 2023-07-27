fe475705d8988852dc1c2a806eb91df0c71d5e8e
Muzammil <muzammilsiddiqui826@gmail.com>
2023-07-27 22:13:26 +0530

    clipboard changes

--- src/components/ClipBoard.jsx
+++ src/components/ClipBoard.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect }  from 'react'
+import { collection, doc, onSnapshot, setDoc } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+import {db, storage} from '../firbase-config'
 
 function ClipBoard() {
+
+  const [sharedText, setSharedText] = useState('');
+
+  // Function to handle text input change
+  const handleChange = (event) => {
+    setSharedText(event.target.value);
+  };
+
+  // Function to save the shared text to Firestore
+  const saveSharedText = async () => {
+    const clipboardDocRef = doc(db, 'clipboard', 'shared_text');
+    await setDoc(clipboardDocRef, { text: sharedText });
+    console.log('text-sent');
+  };
+
+  useEffect(() => {
+    // Set up Firestore listener to get shared text updates
+    const clipboardDocRef = doc(db, 'clipboard', 'shared_text');
+    const unsubscribe = onSnapshot(clipboardDocRef, (doc) => {
+      if (doc.exists()) {
+        const data = doc.data();
+        setSharedText(data.text || '');
+      } else {
+        setSharedText('');
+      }
+    });
+
+    // Clean up the listener on unmount
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+
+
+  
   return (
     <div className='clipboard animate__animated animate__slideInRight animate__faster'>
       <p>Clip-Board</p>
-      <textarea name="text-field" className='text-field' rows="10"></textarea>
+      <textarea name="text-field" className='text-field' rows="10" value={sharedText} onChange={handleChange}></textarea>
       <div className="buttons">
-        <div className="refresh"><i class="fa-solid fa-arrows-rotate"></i>Refresh</div>
+        <div className="refresh" onClick={saveSharedText}><i class="fa-solid fa-arrows-rotate"></i>Refresh</div>
         <div className="clear"><i class="fa-solid fa-eraser"></i>Clear</div>
       </div>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ function Header({ bucketCode, setBucketCode }) {
     <div className="header animate__animated animate__fadeIn">
       <div className="logo">
         <h1>FileGlide</h1>
+        <span>BETA</span>
       </div>
       <HeaderCard bucketCode={bucketCode} setBucketCode={setBucketCode} />
     </div>
